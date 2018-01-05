d8481fb8a99162b53d3358911d81662de561184a
Igor <igor_mec@hotmail.com>
2018-01-05 01:30:25 -0500

    Made a few more database functions. Now much easier to transfer items from one db to another using just search terms

--- web/db_transfers.py
+++ web/db_transfers.py
@@ -1,7 +1,7 @@
 import sqlite3
 
-maindb = sqlite3.connect('C:/Users/Igor/Documents/Dev/projects/IGE/db/MAIN.db')
-mainCur = maindb.cursor()
+#maindb = sqlite3.connect('C:/Users/Igor/Documents/Dev/projects/IGE/db/MAIN.db')
+#c = maindb.cursor()
 
 
 def create_db(name):
@@ -13,7 +13,62 @@ def create_db(name):
     conn.commit()
     conn.close()
 
+def create_table(db, tableName):
+    conn = sqlite3.connect("C:/Users/Igor/Documents/Dev/projects/IGE/db/"+db+".db")
+    print("Connected to "+db)
+    c = conn.cursor()
     
+    c.execute("CREATE TABLE  "+tableName+" (id, username, full_name, profile_pic_url, is_verified, followed_by_viewer, requested_by_viewer)")
+    print("Created table "+tableName+" in "+db)
+    conn.commit()
+    conn.close()
+
+def search(term, db='MAIN', tableName='following'):
+    conn = sqlite3.connect("C:/Users/Igor/Documents/Dev/projects/IGE/db/"+db+".db")
+    print("Connected to "+db)
+    c = conn.cursor()
     
-c.execute("SELECT * FROM following WHERE (username LIKE '%bern%') OR (full_name LIKE '%bern%')")
-rows = c.fetchall()
+    c.execute("SELECT * FROM "+tableName+" WHERE (username LIKE '%"+term+"%') OR (full_name LIKE '%"+term+"%')")
+    rows = c.fetchall()
+    print("Found %d results" % len(rows))
+    
+    conn.commit()
+    conn.close()
+
+    return rows
+
+def deleteItems(db,tableName,term):
+    conn = sqlite3.connect("C:/Users/Igor/Documents/Dev/projects/IGE/db/"+db+".db")
+    print("Connected to "+db)
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM "+tableName+" WHERE (username LIKE '%"+term+"%') OR (full_name LIKE '%"+term+"%')")
+    rows = c.fetchall()
+
+    confirm = input("Delete %d items from %s, %s? (Y/N): " % (len(rows), db, tableName))
+    if confirm == 'Y' or confirm == 'y':
+        c.execute("DELETE FROM "+tableName+" WHERE (username LIKE '%"+term+"%') OR (full_name LIKE '%"+term+"%')")
+        print("Deleted %d items." % len(rows))
+    else:
+        print("Items not deleted.")
+
+    conn.commit()
+    conn.close()
+
+def moveItems(toDB, toTable, term, fromDB='MAIN', fromTable='following'):
+    items = search(term, fromDB, fromTable)
+
+    conn = sqlite3.connect("C:/Users/Igor/Documents/Dev/projects/IGE/db/"+toDB+".db")
+    c = conn.cursor()
+
+    for row in items:
+        print("Inserting item: "+ row[1])
+        c.execute("INSERT INTO "+toTable+" values(?,?,?,?,?,?,?)", row)
+
+    print("Done copying %d items" % len(items))
+    conn.commit()
+    conn.close()
+
+    deleteItems(fromDB, fromTable, term)
+              
+
--- web/getFollowing.py
+++ web/getFollowing.py
@@ -4,10 +4,10 @@ import time
 import sqlite3
 from http.cookies import SimpleCookie
 
-'''
+
 #get cookie from Chrome dev tools and paste here
 #this converts the cookie into proper format for requests
-raw = 'mid=tBf3a-Q"'
+raw = 'mid=Wj-AuAAEAAH1VCWP6a6plRHXSb-Y; fbm_124024574287414=base_domain=.instagram.com; csrftoken=x225iit3aHCMiOwcCdek9eLuzxMQFVQB; ds_user_id=239113428; rur=ASH; sessionid=IGSCdc22d28459fa880bc35be0d77492bb90fdc794556c0ab8e0c6501670e19bd33d%3ARyLgyHSqTt8TQVtRcqiPc6NQeIJD2Ikk%3A%7B%22_auth_user_id%22%3A239113428%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A4%2C%22_token_ver%22%3A2%2C%22_token%22%3A%22239113428%3A0qe9V2bzXuKYEIHHFAQdjj0V0y9TORd2%3Ad4506e6d38da67a7a80b52296893625ed3787c2fc4a2e7b21158a99a3be19715%22%2C%22last_refreshed%22%3A1515041065.4881484509%7D; ig_pr=1; ig_or=landscape-primary; ig_vw=944; ig_vh=922; fbsr_124024574287414=LkYoWVtr9aIqwBZg1FNBFDWRholz6YXHeVGKZanGZKU.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImNvZGUiOiJBUURFUlFNWDh1LVdoZjhPYXZqMGFLLWhKVkthUE1PU19vOFpDaGZoQ3NmVXpXelhnaW5UWUdUREJMRnVUX1FOR0htNEc4MWdCRlM1U0xpd3pRTk1uZXJIZzdpd1YzNFlkcTVuVWNsZEo0dXNSY205aUkzVEx0Rmp5UTNheFdUYktIYVpvLUZFMlp2MG1JUG03RlN3RXlHOEVCejhzQWRVUUdkelF5ZzhEVXJEMkxoVmlCc2UyS3pMTmZqbkNPWENDSDVqUl92c0s2bmhwZVlsNFAwUl8yUXFVRWlwV0tuYmFEUFJXSW5GZnhsU25ybG1QQzdqNEZBeUllX2dtQUNCb3Q3VVRtN2JEdVoxVFAzMVJxOEx1X09EUGt2emxkMjlfcVZMYVNxZG1hdnZEdTl5TUJ2SFpKVVloTEVaeEFQTXkwcDBmekVHbUxVYkxScFVEMFBTbkt0QyIsImlzc3VlZF9hdCI6MTUxNTA0MTE4MCwidXNlcl9pZCI6IjEwNzM0ODUxMjYifQ; urlgen="{\"time\": 1515041064}:1eWxQl:VHfH4qMc_9axVrLpkMThozoVmKU"'
 cookie = SimpleCookie()
 cookie.load(raw)
 cookies = {}
@@ -48,4 +48,4 @@ for user in followersList:
 conn.commit()
 conn.close()
 
-'''
+
