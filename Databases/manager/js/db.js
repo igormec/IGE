6bc33a7493bbcc288568dfe1ce172df3e47ecb3d
unknown <igor_mec@hotmail.com>
2018-09-18 17:38:36 -0400

    Edited some DB code

--- Databases/manager/js/db.js
+++ Databases/manager/js/db.js
@@ -11,51 +11,46 @@ worker.onerror = error;
 
 
 // Open a database
-worker.postMessage({action:'open'});
+worker.postMessage({ action: 'open' });
 
 // Connect to the HTML element we 'print' to
 function print(text) {
-    outputElm.innerHTML = text.replace(/\n/g, '<br>');
+	outputElm.innerHTML = text.replace(/\n/g, '<br>');
 }
 
 function error(e) {
-    console.log(e);
-      errorElm.style.height = '2em';
-      errorElm.textContent = e.message;
+	console.log(e);
+	errorElm.style.height = '2em';
+	errorElm.textContent = e.message;
 }
 
 function noerror() {
-    errorElm.style.height = '0';
+	errorElm.style.height = '0';
 }
 
 
 function execute(commands) {
-	worker.onmessage = function(event) {
+	worker.onmessage = function (event) {
 		var results = event.data.results;
-        console.log("Executing SQL");
+		console.log("Executing SQL");
 
 		outputElm.innerHTML = "";
 		var text = $("#outputElm").html();
 		var lis = []
 		console.log(results);
-		for (var i=0; i<results.length; i++) {
-			console.log(results[i].values[1])
-			console.log(results[i].values[1][1])
+		for (var i = 0; i < results.length; i++) {
+
 			outputElm.appendChild(tableCreate(results[i].columns, results[i].values));
 			makeTable(results[i].values)
-			// console.log(results[i].values);
-			// console.log("hello");
-			// text = $("#outputElm").html();
-			// text = text + "<br>" + results[i];
-			// $("#outputElm").html(text);
+
+			console.log("Displaying results");
 		}
-		console.log("Displaying results");
+
+		var c = "SELECT * FROM following;"
+		worker.postMessage({ action: 'exec', sql: c });
+		outputElm.textContent = "Fetching results...";
 	}
 
-	var c = "SELECT * FROM following;"
-	worker.postMessage({action:'exec', sql:c});
-	outputElm.textContent = "Fetching results...";
-}
 
 
 
@@ -67,38 +62,37 @@ function execute(commands) {
 
 
 
+	function makeTable(dataArr) {
 
-function makeTable(dataArr) {
+		var $table = $("#profTable");
+		$table.remove();
+		$("#main-grid").append($("<table>", { "id": "profTable" }))
+		$table = $("#profTable");
+		$tbody = $("<tbody>");
+		$table.append($tbody);
+		//nuclear option
+		//$table.html("");
+		console.log(dataArr.length);
 
-	var $table = $("#profTable");
-	$table.remove();
-	$("#main-grid").append($("<table>", {"id":"profTable"}))
-	$table = $("#profTable");
-	$tbody = $("<tbody>");
-	$table.append($tbody);
-	//nuclear option
-	//$table.html("");
-	console.log(dataArr.length);
+		//Go through all the nodes at the current BookmarkNode
+		for (var i = 0; i < (dataArr.length); i++) {
+			//Add a new row every 5 nodes, makes 5 columns.
+			if (i % 5 == 0) {
+				var $row = $("<tr>");
+				$tbody.append($row);
+			}
+			//Make a td element
+			//make the mainListItem div inside td
+			var $td = makeProfileDiv(dataArr[i]);
+			//$td.append();
 
-	//Go through all the nodes at the current BookmarkNode
-	for(var i = 0;i < (dataArr.length);i++){
-		//Add a new row every 5 nodes, makes 5 columns.
-		if(i % 5 == 0){
-			var $row = $("<tr>");
-			$tbody.append($row);
+
+			//After adding 5 nodes to row, append row to table
+			$row.append($td);
 		}
-		//Make a td element
-		//make the mainListItem div inside td
-		var $td = makeProfileDiv(dataArr[i]);
-		//$td.append();
-
-
-		//After adding 5 nodes to row, append row to table
-		$row.append($td);
+		//Refresh all the divs functionalities
+		addEvents();
 	}
-	//Refresh all the divs functionalities
-	addEvents();
-}
 
 
 
@@ -106,95 +100,67 @@ function makeTable(dataArr) {
 
 
 
-function makeProfileDiv(prof) {
-	var $itemDiv = $("<td>", {"class":"mainListItem", "id":prof[0]});
-	var $pic = $("<img>", {"src":prof[3],"class":"profimg","width":"85", "height":"85"});
-	var $handle = $("<p>", {"class":"profhandle"}).append(prof[1]);
-	var $name = $("<p>", {"class":"profname"}).append(prof[2]);
+	function makeProfileDiv(prof) {
+		var $itemDiv = $("<td>", { "class": "mainListItem", "id": prof[0] });
+		var $pic = $("<img>", { "src": prof[3], "class": "profimg", "width": "85", "height": "85" });
+		var $handle = $("<p>", { "class": "profhandle" }).append(prof[1]);
+		var $name = $("<p>", { "class": "profname" }).append(prof[2]);
 
-	$itemDiv.append($pic);
-	$itemDiv.append($handle);
-	$itemDiv.append($name);
+		$itemDiv.append($pic);
+		$itemDiv.append($handle);
+		$itemDiv.append($name);
 
-	return $itemDiv;
+		return $itemDiv;
+	}
 
-}
-
-
-
-
-
-
-
+	function execSQL() {
+		noerror()
+		var command = "SELECT `name`, `sql`\n  FROM `sqlite_master`\n  WHERE type='table';";
+		execute(command);
+	}
 
 
 
-// Create an HTML table
-var tableCreate = function () {
-    function valconcat(vals, tagName) {
-      if (vals.length === 0) return '';
-      var open = '<'+tagName+'>', close='</'+tagName+'>';
-      return open + vals.join(close + open) + close;
-    }
-    return function (columns, values){
-      var tbl  = document.createElement('table');
-      var html = '<thead>' + valconcat(columns, 'th') + '</thead>';
-      var rows = values.map(function(v){ return valconcat(v, 'td'); });
-      html += '<tbody>' + valconcat(rows, 'tr') + '</tbody>';
-        tbl.innerHTML = html;
-      return tbl;
-    }
-  }();
+	// Load a db from a file
+	dbFileElm.onchange = function () {
+		console.log("Loading new DB: " + dbFileElm.files[0]);
+		var f = dbFileElm.files[0];
+		var r = new FileReader();
+		r.onload = function () {
+			worker.onmessage = function () {
+				// Show the schema of the loaded database
+				console.log("in OnMessage");
+				execSQL();
+			};
+			//worker.onmessage = function () {};
+			try {
+				console.log(r.result);
+				worker.postMessage({ action: 'open', buffer: r.result }, [r.result]);
+				console.log(r.result);
+			}
+			catch (exception) {
+				console.log("catch");
+				worker.postMessage({ action: 'open', buffer: r.result });
+			}
+		}
+		r.readAsArrayBuffer(f);
+	}
 
-function execSQL () {
-    noerror()
-    var command = "SELECT `name`, `sql`\n  FROM `sqlite_master`\n  WHERE type='table';";
-	execute (command);
-}
-
-
-
-// Load a db from a file
-dbFileElm.onchange = function() {
-    console.log("new DB detected");
-	var f = dbFileElm.files[0];
-	var r = new FileReader();
-	r.onload = function(){
-		worker.onmessage = function () {
-			// Show the schema of the loaded database
-			console.log("in OnMessage")			;
-			execSQL();
+	// Save the db to a file
+	function savedb() {
+		worker.onmessage = function (event) {
+			var arraybuff = event.data.buffer;
+			var blob = new Blob([arraybuff]);
+			var a = document.createElement("a");
+			a.href = window.URL.createObjectURL(blob);
+			a.download = "sql.db";
+			a.onclick = function () {
+				setTimeout(function () {
+					window.URL.revokeObjectURL(a.href);
+				}, 1500);
+			};
+			a.click();
 		};
-		//worker.onmessage = function () {};
-		try {
-			console.log(r.result);
-			worker.postMessage({action:'open',buffer:r.result}, [r.result]);
-			console.log(r.result);
-		}
-		catch(exception) {
-			console.log("catch");
-			worker.postMessage({action:'open',buffer:r.result});
-		}
+		worker.postMessage({ action: 'export' });
 	}
-	console.log("end");
-	r.readAsArrayBuffer(f);
-}
-
-// Save the db to a file
-function savedb () {
-	worker.onmessage = function(event) {
-		var arraybuff = event.data.buffer;
-		var blob = new Blob([arraybuff]);
-		var a = document.createElement("a");
-		a.href = window.URL.createObjectURL(blob);
-		a.download = "sql.db";
-		a.onclick = function() {
-			setTimeout(function() {
-				window.URL.revokeObjectURL(a.href);
-			}, 1500);
-		};
-		a.click();
-	};
-	worker.postMessage({action:'export'});
-}
-savedbElm.addEventListener("click", savedb, true);
+	savedbElm.addEventListener("click", savedb, true);
